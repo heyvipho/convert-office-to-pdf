0c547babc8b65c9a593faf799abb1c1647032634
Thuong Nguyen Van <nvthuong1996@gmail.com>
2021-11-29 16:08:05 +0700

    update

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -10,39 +9,45 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"time"
+	"math/rand"
 )
 
+var channel = make(chan int, 10)
+
+
 func main() {
-	log.Println("==============")
-	log.Println(os.TempDir())
+	channel <- 1
 	http.HandleFunc("/convert", handler)
 	http.ListenAndServe(":3000", nil)
 }
 
-func downloadFile(path string, url string) (error) {
+func downloadFile(path string, url string) error {
 
 	// Create the file
 	out, err := os.Create(path)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	defer out.Close()
 
 	// Get the data
 	resp, err := http.Get(url)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	defer resp.Body.Close()
 
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
-
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	log.Println("request incoming")
 
 	keys, ok := r.URL.Query()["fileSrc"]
 
@@ -54,24 +59,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	fileSrc := keys[0]
 
+	log.Println("request incoming with fileSrc:",fileSrc)
+
 	workDir := os.TempDir()
 
-	fileName := "document" + fmt.Sprint(time.Now().Unix())
+	fileName := "document" + fmt.Sprint(rand.Intn(100000))
 
-	inputFile := filepath.Join(workDir,fileName)
+	inputFile := filepath.Join(workDir, fileName)
 
-	outputFile := filepath.Join(workDir, fileName + ".pdf")
+	outputFile := filepath.Join(workDir, fileName+".pdf")
 
-	errorDownload := downloadFile(inputFile,fileSrc)
+	errorDownload := downloadFile(inputFile, fileSrc)
 
-	if(errorDownload != nil){
+	if errorDownload != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("error when download file"))
 		return
 	}
 
-	cmd := exec.Command("libreoffice","--headless","--convert-to" ,"pdf","--outdir" , workDir,  inputFile )
-	_,err := cmd.Output()
+	<- channel
+	cmd := exec.Command("libreoffice", "--headless", "--convert-to", "pdf", "--outdir", workDir, inputFile)
+	_, err := cmd.Output()
+	channel <- 1
+
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -85,6 +95,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fileBytes, err := ioutil.ReadFile(outputFile)
 
 	w.Write(fileBytes)
+
+	os.Remove(outputFile)
+	os.Remove(inputFile)
 
 	return
 }
---
+++ readme.md
@@ -0,0 +1,2 @@
+Convert document to pdf
+
